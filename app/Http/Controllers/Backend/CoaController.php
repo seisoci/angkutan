18ac27924031c3f3d50c0b24b2c5ca263794710c
Eclair <kurnia.okta@gmail.com>
2021-07-26 00:34:18 +0700

    update coa

--- app/Http/Controllers/Backend/CoaController.php
+++ app/Http/Controllers/Backend/CoaController.php
@@ -4,7 +4,23 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Coa;
+use App\Models\InvoiceCostumer;
+use App\Models\InvoiceKasbon;
+use App\Models\InvoiceKasbonEmployee;
+use App\Models\InvoiceLdo;
+use App\Models\InvoicePurchase;
+use App\Models\InvoiceReturPurchase;
+use App\Models\InvoiceSalary;
+use App\Models\InvoiceUsageItem;
+use App\Models\JobOrder;
+use App\Models\Journal;
+use App\Models\Kasbon;
+use App\Models\KasbonEmployee;
+use App\Models\MonthlySalaryDetail;
+use App\Models\OperationalExpense;
+use App\Models\UsageItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CoaController extends Controller
@@ -109,6 +125,165 @@ class CoaController extends Controller
       ]);
     } else {
       $response = response()->json(['error' => $validator->errors()->all()]);
+    }
+    return $response;
+  }
+
+  public function update(Request $request, $id)
+  {
+    $validator = Validator::make($request->all(), [
+      'name' => 'required|string',
+    ]);
+
+    if ($validator->passes()) {
+      $data = Coa::find($id);
+      $data->update([
+        'name' => $request->input('name'),
+      ]);
+      $response = response()->json([
+        'status' => 'success',
+        'message' => 'Data has been saved',
+        'redirect' => 'reload'
+      ]);
+    } else {
+      $response = response()->json(['error' => $validator->errors()->all()]);
+    }
+    return $response;
+  }
+
+  public function destroy($id)
+  {
+    $response = response()->json([
+      'status' => 'error',
+      'message' => 'Data cannot be deleted',
+    ]);
+    $coa = Coa::with('children')->findOrFail($id);
+
+    try {
+      DB::beginTransaction();
+      $journal = Journal::where('coa_id', $coa->id)->get();
+      foreach ($journal as $item):
+        Journal::where('table_ref', $item->table_ref)->where('code_ref', $item->code_ref)->delete();
+        if ($item->table_ref === 'joborders') {
+          $data = JobOrder::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'operationalexpense') {
+          $data = OperationalExpense::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'invoicepurchases') {
+          $dataPurchase = InvoicePurchase::find($item->code_ref);
+          if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+          $usageItem = UsageItem::where('invoice_purchase_id', $item->code_ref)->get();
+          foreach ($usageItem as $usage) {
+            $data = InvoiceUsageItem::find($usageItem->invoice_usage_item_id)->delete();
+            if ($data != NULL): $data->delete(); endif;
+          }
+        } elseif ($item->table_ref === 'invoiceusageitems') {
+          $data = InvoiceUsageItem::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'invoiceusageitemsoutside') {
+          $data = InvoiceUsageItem::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'kasbon') {
+          $data = Kasbon::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'invoicekasbons') {
+          $data = InvoiceKasbon::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'kasbonemployees') {
+          $data = KasbonEmployee::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'invoicekasbonemployees') {
+          $data = InvoiceKasbonEmployee::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'invoicesalaries') {
+          $data = InvoiceSalary::find($item->code_ref);
+          if ($data != NULL): $data->delete(); endif;
+        } elseif ($item->table_ref === 'monthlysalarydetail') {
+          $data = MonthlySalaryDetail::find($item->code_ref);
+          if ($data != NULL): $data->update(['status' => '0']); endif;
+        } elseif ($item->table_ref === 'invoicecostumers') {
+          $data = InvoiceCostumer::find($item->code_ref);
+          if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+        } elseif ($item->table_ref === 'invoiceldo') {
+          $data = InvoiceLdo::find($item->code_ref);
+          if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+        } elseif ($item->table_ref === 'invoicereturpurchases') {
+          $data = InvoiceReturPurchase::find($item->code_ref);
+          if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+        }
+      endforeach;
+      $coa->delete();
+      //Children COA
+      foreach ($coa->children as $itemCoa):
+        $coaChildren = Coa::findOrFail($itemCoa->id);
+        $journalChildren = Journal::where('coa_id', $coaChildren->id)->get();
+        foreach ($journalChildren as $item) {
+          Journal::where('table_ref', $item->table_ref)->where('code_ref', $item->code_ref)->delete();
+          if ($item->table_ref === 'joborders') {
+            $data = JobOrder::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'operationalexpense') {
+            $data = OperationalExpense::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'invoicepurchases') {
+            $dataPurchase = InvoicePurchase::find($item->code_ref);
+            if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+            $usageItem = UsageItem::where('invoice_purchase_id', $item->code_ref)->get();
+            foreach ($usageItem as $usage) {
+              $data = InvoiceUsageItem::find($usageItem->invoice_usage_item_id)->delete();
+              if ($data != NULL): $data->delete(); endif;
+            }
+          } elseif ($item->table_ref === 'invoiceusageitems') {
+            $data = InvoiceUsageItem::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'invoiceusageitemsoutside') {
+            $data = InvoiceUsageItem::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'kasbon') {
+            $data = Kasbon::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'invoicekasbons') {
+            $data = InvoiceKasbon::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'kasbonemployees') {
+            $data = KasbonEmployee::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'invoicekasbonemployees') {
+            $data = InvoiceKasbonEmployee::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'invoicesalaries') {
+            $data = InvoiceSalary::find($item->code_ref);
+            if ($data != NULL): $data->delete(); endif;
+          } elseif ($item->table_ref === 'monthlysalarydetail') {
+            $data = MonthlySalaryDetail::find($item->code_ref);
+            if ($data != NULL): $data->update(['status' => '0']); endif;
+          } elseif ($item->table_ref === 'invoicecostumers') {
+            $data = InvoiceCostumer::find($item->code_ref);
+            if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+          } elseif ($item->table_ref === 'invoiceldo') {
+            $data = InvoiceLdo::find($item->code_ref);
+            if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+          } elseif ($item->table_ref === 'invoicereturpurchases') {
+            $data = InvoiceReturPurchase::find($item->code_ref);
+            if ($dataPurchase != NULL): $dataPurchase->delete(); endif;
+          }
+        }
+        $coaChildren->delete();
+      endforeach;
+      DB::commit();
+      $response = response()->json([
+        'status' => 'success',
+        'message' => 'Data has been deleted',
+        'redirect' => 'reload'
+      ]);
+    } catch (\Throwable $throw) {
+      DB::rollBack();
+      $response = $throw;
+//      $response = response()->json([
+//        'status' => 'error',
+//        'message' => 'Data cannot be deleted',
+//      ]);
     }
     return $response;
   }
--- resources/views/backend/masterfinance/coa/index.blade.php
+++ resources/views/backend/masterfinance/coa/index.blade.php
@@ -53,20 +53,81 @@
               <td>{{ $itemParent->code }}</td>
               <td>{{ $itemParent->name }}</td>
               <td>{{ $itemParent->normal_balance }}</td>
-              <td></td>
+              <td><a href="#" data-toggle="modal" data-target="#modalEdit" data-id="{{ $itemParent->id }}"
+                     data-name="{{ $itemParent->name }}" class="edit btn btn-warning btn-sm">Edit</a>
+              {!! $itemParent->can_delete == '1' ? '<a href="#" data-toggle="modal" data-target="#modalDelete" data-id="'.$itemParent->id.'" class="delete btn btn-danger btn-sm">Delete</a>' : NULL !!}
+
             </tr>
             @foreach($itemParent->children as $itemChildren)
               <tr class="font-weight-bold">
                 <td>{{ $itemChildren->code }}</td>
                 <td>{{ $itemChildren->name }}</td>
                 <td>{{ $itemChildren->normal_balance }}</td>
-                <td></td>
+                <td><a href="#" data-toggle="modal" data-target="#modalEdit" data-id="{{ $itemChildren->id }}"
+                       data-name="{{ $itemChildren->name }}" class="edit btn btn-warning btn-sm">Edit</a>
+                  {!! $itemChildren->can_delete == '1' ? '<a href="#" data-toggle="modal" data-target="#modalDelete" data-id="'.$itemChildren->id.'" class="delete btn btn-danger btn-sm">Delete</a>' : NULL !!}
+                </td>
               </tr>
             @endforeach
           @endforeach
         @endforeach
         </tbody>
       </table>
+    </div>
+  </div>
+  <div class="modal fade" id="modalEdit" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Edit {{ $config['page_title'] }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <i aria-hidden="true" class="ki ki-close"></i>
+          </button>
+        </div>
+        <form id="formUpdate" action="#">
+          @method('PUT')
+          <meta name="csrf-token" content="{{ csrf_token() }}">
+          <div class="modal-body">
+            <div class="form-group" style="display:none;">
+              <div class="alert alert-custom alert-light-danger" role="alert">
+                <div class="alert-icon"><i class="flaticon-danger text-danger"></i></div>
+                <div class="alert-text">
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label>Nama Coa</label>
+              <input type="text" name="name" class="form-control form-control-solid" placeholder="Input Nama Brand"/>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="modalDelete" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Delete</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <i aria-hidden="true" class="ki ki-close"></i>
+          </button>
+        </div>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        @method('DELETE')
+        <div class="modal-body">
+          <a href="" type="hidden" name="id" disabled></a>
+          Are you sure you want to delete this item?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button id="formDelete" type="button" class="btn btn-danger">Submit</button>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
@@ -78,11 +139,120 @@
 
 {{-- Scripts Section --}}
 @section('scripts')
-  {{-- vendors --}}
-  <script src="{{ asset('plugins/custom/datatables/datatables.bundle.js') }}" type="text/javascript"></script>
   {{-- page scripts --}}
   <script type="text/javascript">
     $(document).ready(function () {
+
+      $('#modalEdit').on('show.bs.modal', function (event) {
+        let id = $(event.relatedTarget).data('id');
+        let name = $(event.relatedTarget).data('name');
+        $(this).find('#formUpdate').attr('action', '{{ route("backend.mastercoa.index") }}/' + id)
+        $(this).find('.modal-body').find('input[name="name"]').val(name);
+      });
+      $('#modalEdit').on('hidden.bs.modal', function (event) {
+        $(this).find('.modal-body').find('input[name="name"]').val('');
+      });
+
+      $('#modalDelete').on('show.bs.modal', function (event) {
+        let id = $(event.relatedTarget).data('id');
+        $(this).find('.modal-body').find('a[name="id"]').attr('href', '{{ route("backend.mastercoa.index") }}/' + id);
+      });
+
+      $('#modalDelete').on('hidden.bs.modal', function (event) {
+        $(this).find('.modal-body').find('a[name="id"]').attr('href', '');
+      });
+
+      $("#formUpdate").submit(function (e) {
+        e.preventDefault();
+        let form = $(this);
+        let btnSubmit = form.find("[type='submit']");
+        let btnSubmitHtml = btnSubmit.html();
+        let spinner = $('<span role="status" class="spinner-border spinner-border-sm" aria-hidden="true"></span>');
+        let url = form.attr("action");
+        let data = new FormData(this);
+        $.ajax({
+          beforeSend: function () {
+            btnSubmit.addClass("disabled").html("<i class='fa fa-spinner fa-pulse fa-fw'></i> Loading...").prop("disabled", "disabled");
+          },
+          headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          },
+          cache: false,
+          processData: false,
+          contentType: false,
+          type: "POST",
+          url: url,
+          data: data,
+          success: function (response) {
+            btnSubmit.removeClass("disabled").html(btnSubmitHtml).removeAttr("disabled");
+            if (response.status === "success") {
+              toastr.success(response.message, 'Success !');
+              setTimeout(function () {
+                if (response.redirect === "" || response.redirect === "reload") {
+                  location.reload();
+                } else {
+                  location.href = response.redirect;
+                }
+              }, 1000);
+              $('#modalEdit').modal('hide');
+              $("[role='alert']").parent().css("display", "none");
+            } else {
+              $("[role='alert']").parent().removeAttr("style");
+              $(".alert-text").html('');
+              $.each(response.error, function (key, value) {
+                $(".alert-text").append('<span style="display: block">' + value + '</span>');
+              });
+              toastr.error("Please complete your form", 'Failed !');
+            }
+          }, error: function (response) {
+            btnSubmit.removeClass("disabled").html(btnSubmitHtml).removeAttr("disabled");
+            toastr.error(response.responseJSON.message, 'Failed !');
+            $('#modalEdit').modal('hide');
+            $('#modalEdit').find('a[name="id"]').attr('href', '');
+          }
+        });
+      });
+
+      $("#formDelete").click(function (e) {
+        e.preventDefault();
+        let form = $(this);
+        let url = $('#modalDelete').find('a[name="id"]').attr('href');
+        let btnHtml = form.html();
+        let spinner = $('<span role="status" class="spinner-border spinner-border-sm" aria-hidden="true"></span>');
+        $.ajax({
+          beforeSend: function () {
+            form.prop('disabled', true).html("<i class='fa fa-spinner fa-pulse fa-fw'></i> Loading...");
+          },
+          type: 'DELETE',
+          url: url,
+          dataType: 'json',
+          headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+          success: function (response) {
+            if (response.status === "success") {
+              form.prop('disabled', false).html(btnHtml);
+              toastr.success(response.message, 'Success !');
+              $('#modalDelete').modal('hide');
+              setTimeout(function () {
+                if (response.redirect === "" || response.redirect === "reload") {
+                  location.reload();
+                } else {
+                  location.href = response.redirect;
+                }
+              }, 1000);
+            } else {
+              form.prop('disabled', false).html(btnHtml);
+              toastr.error(response.message, 'Failed !');
+              $('#modalDelete').modal('hide');
+            }
+          },
+          error: function (response) {
+            form.prop('disabled', false).text('Submit').find("[role='status']").removeClass("spinner-border spinner-border-sm").html(btnHtml);
+            toastr.error(response.responseJSON.message, 'Failed !');
+            $('#modalDelete').modal('hide');
+            $('#modalDelete').find('a[name="id"]').attr('href', '');
+          }
+        });
+      });
     });
   </script>
 @endsection
